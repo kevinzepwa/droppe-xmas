6b4280dd9b9353847d7bccfc87ffa0f5c400c500
kevinzepwa <zepwakevin8@gmail.com>
2022-01-23 08:22:43 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="App-header">
+        <a href="/">Droppe X-mas</a> 
+      </header>
+      <main>
+        Product list
+      </main>
+      <footer>
+        brewed by devKev
+      </footer>
+    </div>
+  );
+}
+
+export default App;
